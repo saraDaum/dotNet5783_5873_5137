7cd3fc0ea8ca1663d80db8e8dceffab95920502a
saraDaum <sara.daum.il@gmail.com>
2022-10-30 10:07:37 +0200

    We

---
+++ dotNet5782_5873/stage0/Program5873.cs
@@ -0,0 +1,25 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System;
+
+namespace stage0
+{
+    partial class Program
+    {
+        static void Main (string[] rags)
+        {
+            welcome5873();
+            welcome5874();
+            Console.ReadKey();
+        }
+
+        private static void welcome5873()
+        {
+            Console.Write("Enter your name, Please.");
+            string userName = Console.ReadLine();
+            Console.WriteLine("{0}, welcome to my first console application.", userName);
+        }
+        static partial void welcome5874() { }
+    }
+
+}
+
