819858190c15cd06417da5b031090b9ec97ac9db
MyVirtualFreind <library.project.me@gmail.com>
2022-11-07 00:45:24 +0200

    I did something

--- dotNet5782_5873/DAL_List/DataSource.cs
+++ dotNet5782_5873/DAL_List/DataSource.cs
@@ -9,4 +9,9 @@ internal static class DataSource
     internal static Order[] OrderArray = new Order[100];
     internal static OrderItem[] orderItemArray = new OrderItem[200];
     internal static Product[] productArray = new Product[50];
+
+    private static void init_order()
+    {
+        ID
+    }
  }
