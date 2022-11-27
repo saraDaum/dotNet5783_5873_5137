667a10facfb427268ab2bcf2105502dbb85b6878
saraDaum <sara.daum.il@gmail.com>
2022-11-27 22:43:14 +0200

    I added classes to BlImplementation folder

---
+++ dotNet5782_5873/BL/BlImplementation/Product.cs
@@ -0,0 +1,11 @@
+﻿using BlApi;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BlImplementation;
+internal class Product:IProduct
+{
+}
