20502b33bf303079a8c3d8ee0d2a8d4f1d2544a9
SaraDaum <sara.daum.il@users.noreply.github.com>
2023-01-05 21:13:30 +0200

    Changes

---
+++ dotNet5782_5873/BL/BlImplementation - Copy/BoOrderForList.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using BlApi;
+using BO;
+using DAL;
+using DalApi;
+
+namespace BlImplementation;
+
+internal class BoOrderForList:IBoOrderForList
+{
+    private IDal dal= new DalList();
+    
+   
+    public int Add(Order entity)
+    {
+        throw new NotImplementedException();
+    }
+
+    public void Delete(int id)
+    {
+        throw new NotImplementedException();
+    }
+
+    public IEnumerable<Order> GetAll()
+    {
+        throw new NotImplementedException();
+    }
+
+    public Order GetById(int id)
+    {
+        throw new NotImplementedException();
+    }
+
+    public void Update(Order entity)
+    {
+        throw new NotImplementedException();
+    }
+
+    // להמיר
+    // ואז
+    // Dal.Order.Add(DO.Order O)
+
+
+}
---
+++ dotNet5782_5873/BL/BlImplementation - Copy/BoProductItem.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using BlApi;
+using DAL;
+using DalApi;
+
+namespace BlImplementation;
+internal class BoProductItem:IBoProductItem
+{
+    private IDal dal= new DalList();
+
+    public int Add(BO.ProductItem entity)
+    {
+        throw new NotImplementedException();
+    }
+
+    public void Delete(int id)
+    {
+        throw new NotImplementedException();
+    }
+
+    public IEnumerable<BO.ProductItem> GetAll()
+    {
+        throw new NotImplementedException();
+    }
+
+    public BO.ProductItem GetById(int id)
+    {
+        throw new NotImplementedException();
+    }
+
+    public void Update(BO.ProductItem entity)
+    {
+        throw new NotImplementedException();
+    }
+}
